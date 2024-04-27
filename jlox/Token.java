d59b21e901db0db8c40277305db9ad754b2977db
Vinicius Simizu <viniciussimiz@gmail.com>
2024-04-27 19:41:47 -0300

    Tokens

---
+++ jlox/Token.java
@@ -0,0 +1,13 @@
+public class Token {
+	private TokenType type;
+	private String lexeme;
+	private Object literal;
+	private int line;
+
+	public Token(TokenType type, String lexeme, Object literal, int line) {
+		this.type = type;
+		this.lexeme = lexeme;
+		this.literal = literal;
+		this.line = line;
+	};
+};
---
+++ jlox/TokenType.java
@@ -0,0 +1,20 @@
+public enum TokenType {
+	// Single character token
+	OPEN_PAREN, CLOSE_PAREN, OPEN_BRACE, CLOSE_BRACE,
+	COMMA, DOT, PLUS, MINUS, STAR, SLASH, SEMICOLON,
+
+	// Single or doubly character token
+	EQUAL, EQUAL_EQUAL,
+	BANG, BANG_EQUAL,
+	GREATER, GREATER_EQUAL, 
+	LESS, LESS_EQUAL, 
+
+	// Literal
+	LITERAL, STRING, NUMBER,
+
+	// Keywords
+	PRINT, AND, OR, IF, ELSE, FOR, WHILE, FUN, RETURN,
+	CLASS, THIS, SUPER, TRUE, FALSE, VAR, NIL,
+
+	EOF;
+};
